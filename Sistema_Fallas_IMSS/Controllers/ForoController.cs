a70396b379ca844cf4b8b05412522b9c11246a5b
AleCG0101 <131492006+AleCG0101@users.noreply.github.com>
2023-10-22 21:03:10 -0600

    Registrar Foro, validaciones

--- Sistema_Fallas_IMSS/Controllers/ForoController.cs
+++ Sistema_Fallas_IMSS/Controllers/ForoController.cs
@@ -13,8 +13,13 @@ namespace Sistema_Fallas_IMSS.Controllers
         // GET: Foro
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult PartialIndex()
+        {
             List<VM_Foro> data = ObtenerEntradasForo();
-            return View(data);
+            return PartialView("_PartialIndex",data);
         }
 
         public List<VM_Foro> ObtenerEntradasForo()
@@ -30,7 +35,7 @@ namespace Sistema_Fallas_IMSS.Controllers
                     Solucion = foro.solucion,
                     Autor = foro.autor,
                     Correo = foro.correo,
-                }).ToList();
+                }).OrderByDescending(x=> x.Id_foro).ToList();
 
                 return data;
             }
@@ -38,7 +43,85 @@ namespace Sistema_Fallas_IMSS.Controllers
         [HttpPost]
         public ActionResult AbrirModalForo(int id_foro)
         {
+            using (var context = new IMSSEntities())
+            {
+                VM_Foro data = new VM_Foro();
 
+                if (id_foro > 0)
+                {
+                    var foro = context.foro_soluciones.Find(id_foro);
+                    if (foro != null)
+                    {
+                        data = new VM_Foro
+                        {
+                            Id_foro = foro.Id_solucion,
+                            Titutlo = foro.titulo,
+                            Problema = foro.problema,
+                            Solucion = foro.solucion,
+                            Autor = foro.autor,
+                            Correo = foro.correo,
+                        };
+
+                        return PartialView("_ModalRegistrarForo",data);
+                    }                   
+                }
+                return PartialView("_ModalRegistrarForo", data);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult GenerarRegistroForo(VM_Foro _data)
+        {
+            if (!ModelState.IsValid)
+            {
+                _data.Mensaje = "validacion";
+                return PartialView("_ModalRegistrarForo", _data);
+            }
+                
+            using (var context = new IMSSEntities())
+            {
+                using (var transaction = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var foro = context.foro_soluciones.Find(_data.Id_foro);
+                        if (foro != null)
+                        {
+                            foro.titulo = _data.Titutlo;
+                            foro.problema = _data.Problema;
+                            foro.solucion = _data.Solucion;
+                            foro.autor = _data.Autor;
+                            foro.correo = _data.Correo;
+                        }
+                        else
+                        {
+                            foro_soluciones newForo = new foro_soluciones
+                            {
+                                titulo = _data.Titutlo,
+                                problema = _data.Problema,
+                                solucion = _data.Solucion,
+                                autor = _data.Autor,
+                                correo = _data.Correo,
+                            };
+                            context.foro_soluciones.Add(newForo);
+                            context.SaveChanges();
+                        }                       
+                        transaction.Commit();
+                        _data.Mensaje = "okay";
+                        return PartialView("_ModalRegistrarForo", _data);
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        _data.Mensaje = "error";
+                        return PartialView("_ModalRegistrarForo", _data);
+                        throw;
+                    }
+
+                }
+
+            }
+            
         }
     }
 }
--- Sistema_Fallas_IMSS/ViewModels/VM_Foro.cs
+++ Sistema_Fallas_IMSS/ViewModels/VM_Foro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,16 @@ namespace Sistema_Fallas_IMSS.ViewModels
     public class VM_Foro
     {
         public int Id_foro { get; set; }
+        [Required] 
         public string Titutlo { get; set; }
+        [Required]
         public string Problema { get; set; }
+        [Required]
         public string Solucion { get; set; }
+        [Required]
         public string Autor { get; set; }
+        [Required]
         public string Correo { get; set; }
+        public string Mensaje { get; set; }
     }
 }
