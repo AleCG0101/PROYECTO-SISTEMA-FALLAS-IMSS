e1c1f95a1216adb045b421142e9d3301c682d88a
AleCG0101 <131492006+AleCG0101@users.noreply.github.com>
2023-11-01 16:29:21 -0600

    ajustes a BD

--- SISTEMA_FALLAS_IMSS.sql
+++ SISTEMA_FALLAS_IMSS.sql
@@ -212,3 +212,5 @@ INSERT INTO fallas(Id_tipo_falla, descripcion) VALUES
 
 INSERT INTO foro_soluciones(titulo, problema, solucion, autor, correo) VALUES
 ('Problema de prueba','Este es el problema de una entrada de prieba','El problema puede corresponder a su conexion a internet, no se estan introducioendo todos los datos necesarios o hay alguna falla interna, favor de acudir personalmente con el encargado del area de sistemas para identificar el caso especifico', 'Victor Manuel Lopez', 'victor.lopez@outlook.com')
+
+select * from reporte
